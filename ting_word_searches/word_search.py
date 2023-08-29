199cf7643b2b4e06c8ed3c9cced9a5c944bdae06
heidegomes <heidegc@hotmail.com>
2023-08-29 18:28:01 -0300

    Requisito 8 feito!

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,4 +21,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    lista = []
+
+    for i in range(len(instance)):
+        result = instance.search(i)
+
+        dicionario = {
+            "palavra": word,
+            "arquivo": result["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for i, linha in enumerate(result["linhas_do_arquivo"]):
+            if word.lower() in linha.lower():
+                dicionario["ocorrencias"].append(
+                    {"linha": i + 1, "conteudo": linha}
+                )
+
+        if len(dicionario["ocorrencias"]):
+            lista.append(dicionario)
+
+    return lista
